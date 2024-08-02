e473c5dbf4e9a4379e296b1d4051904e3797947c
Mike Bell <mbell@albionresearch.com>
2024-08-02 14:18:36 -0400

    Use API call to get camera locations directly

--- README.md
+++ README.md
@@ -15,13 +15,12 @@ repository.
 
 ## Files not up-to-date and know a little Python?
 
-  1. Download The current `Automated_Speed_Enforcement_Camera_Locations.csv` from 
-    [https://open.ottawa.ca/datasets/ottawa::automated-speed-enforcement-camera-locations/explore](https://open.ottawa.ca/datasets/ottawa::automated-speed-enforcement-camera-locations/explore)
-  2. Clone this repo and cd into the repo directory.
-  3. Install the `pyov2` package.
-  4. Run `python speedcams.py` to generate an updated `Speed_Cameras.ov2` file   
-  5. If you know what you're doing, Issue a pull request 
-  6. If you're not quite sure, create an issue requesting an update.
+  1. Clone this repo and cd into the repo directory.
+  2. Install the `pyov2` package.
+  3. Run `python speedcams.py` to generate an updated `Speed_Cameras.ov2` file   
+  4. If you know what you're doing, Issue a pull request 
+  5. If you're not quite sure, create an issue requesting an update.
+  6. API call not working - see below.
 
 ## If you have graphic design abilities...
 
@@ -32,6 +31,25 @@ a bit like a red camera would be much better.
 
 Don't use any clever bitmap formats - they won't work on all devices.
 
+## Got a Garmin or another make?
+
+ 1. Update `speedcams.py` to generate the correct CSV or other format files. 
+ 2. Submit a pull request.
+
+## License
+
+This software is released under the MIT license - see `LICENSE.txt`
+
 ## Last Speed Camera Update
 2024-05-16
+
+## API Call out of date?
+
+The API URL in the Python script can be obtained as follows.
+ 
+ 1. Go to [https://open.ottawa.ca/datasets/ottawa::automated-speed-enforcement-camera-locations]
+ 2. Click on *I want to use this...* then *View API Resources*
+ 3. Copy the GeoJSON API call: [https://services.arcgis.com/G6F8XLCl5KtAlZ2G/arcgis/rest/services/Automated_Speed_Enforcement_Camera_Locations/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson](API Call)
+ 4. Update the `speedcams.py` `API_URL` value in `speedcams.py`.
+ 5. Submit a pull request for the updated URL and the updated data.
 
--- speedcams.py
+++ speedcams.py
@@ -1,63 +1,53 @@
+#
+# speedcams.py
+# 
+# A Python script to generate files which can be imported into a GPS to 
+# warn if you are approaching a speed camera location
+#
+# See the README file for more information
+#
+# Mike Bell, Albion Research Ltd., 2024
+#
+# NO WARRANTIES - USE ENTIRELY AT OWN RISK
+#
 
-import csv
-from pyov2.file import list_to_ov2 
+
+
+
+import urllib.request,json
+from pathlib import Path
+
 from pyov2 import ov2
 
+# See the README.md file for how to get the API URL
 
+API_URL = "https://services.arcgis.com/G6F8XLCl5KtAlZ2G/arcgis/rest/services/Automated_Speed_Enforcement_Camera_Locations/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
 
+def get_json(url):
+    with urllib.request.urlopen(url) as url:
+        data = json.loads(url.read().decode())
+        return data 
 
-def csv_to_ov2(csv_path, ov2_path, skip_header=True, dialect='excel', lat=3,lon=4,id=0,prefix='Camera '):
-    """
-        Convert a csv to an ov2 file.
-        The csv should consist of at least three columns.
-        Use dialect parameter to fit your csv.
+def generate_files( data: str, ov2_path: Path ):
+  if not ov2_path.endswith('.ov2'):
+      ov2_path += '.ov2'
 
-        Parameters
-        ----------
-        csv_path : str
-            Path of csv file to convert.
-        ov2_path : str
-            Path where to save ov2 file.
-        skip_header : bool
-            If the csv contains headers use True (default) otherwise use False.
-        dialect : str
-            Default is 'semicolon' but you can use 'excel' for comma seperated csv.
-        lat : 
-           Is the latitude column index (default is 3)
-        lon : 
-            Is the longitude column index (default is 4)
-        id :
-            Is the label suffix column index (default is 0)
-    """
+  # TomTom devices
+  with open(ov2_path, 'wb') as target_file: 
+      for feature in data['features']:
+          longitude = feature['properties']['Longitude']
+          latitude = feature['properties']['Latitude']
+          label = feature['properties']['ID']
+          buff = ov2.serialize(longitude, latitude, F"Camera {label}")
+          target_file.write(buff)
+  print(f"Wrote {len(data['features'])} camera locations to {ov2_path}")
 
-    if not ov2_path.endswith('.ov2'):
-        ov2_path += '.ov2'
-
-    count = 0
-    with open(csv_path, 'r', newline='') as in_file:
-        with open(ov2_path, 'wb') as target_file:
-            reader = csv.reader(in_file, dialect=dialect)
-
-            if skip_header:
-                next(reader)
-
-            for row in reader:
-                longitude = float(row[lon])
-                latitude = float(row[lat])
-                label = f"{prefix}{row[id]}"
-                #print( f"lon={longitude}, lat={latitude}, label={label}")
-                buff = ov2.serialize(longitude, latitude, label.strip())
-                target_file.write(buff)
-                count += 1
-
-            print(f"Wrote {count} records to {ov2_path}")
-
-
-# The Automated_Speed_Enforcement_Camera_Locations.csv file is available at:
-# https://open.ottawa.ca/datasets/ottawa::automated-speed-enforcement-camera-locations/explore
+def main():
+    data = get_json(API_URL)
+    generate_files(data, 'Speed_Cameras.ov2')# G
 
 
 if __name__ == '__main__':
-    csv_to_ov2('Automated_Speed_Enforcement_Camera_Locations.csv', 'Speed_Cameras.ov2', lat=3, lon=4, id=0, prefix='Camera ')
+    main()
 
 
